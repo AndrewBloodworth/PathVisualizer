21216f4197774d759442b833626711bacefefda5
AndrewBloodworth <andrewcbld@gmail.com>
2021-06-02 23:46:09 -0600

    finished the new settings dropdown

--- src/components/nav/Settings.js
+++ src/components/nav/Settings.js
@@ -29,6 +29,68 @@ export const Settings = ({ numberOfRows, setNumberOfRows }) => {
   const handleMouseLeaveSettings = () => {
     setActive(false);
   };
+  const handleClick = () => {
+    console.log("her");
+    setActive(active ? false : true);
+  };
+  let settingsMenu = (
+    <div
+      className="settings-menu"
+      style={{ visibility: active ? "visible" : "hidden" }}
+      onMouseLeave={handleMouseLeaveSettings}
+    >
+      <div className="settings">
+        <div className="settings-title">
+          <h2 style={{ margin: 0 }}>Settings</h2>
+        </div>
+
+        <hr />
+        <div className="slider">
+          <label for="slider">
+            <span style={{ fontSize: 20 }}>Rows: </span>
+          </label>
+          <input
+            onChange={handleChange}
+            type="range"
+            value={numberOfRows}
+            id="slider"
+            min="5"
+            max="20"
+          ></input>
+        </div>
+        <hr />
+        <div className="speed">
+          <label for="speed">
+            <span style={{ fontSize: 20 }}>Speed: </span>
+          </label>
+          <input
+            onChange={handleChangeSpeed}
+            type="range"
+            value={speed}
+            id="speed"
+            name="speed"
+            min="10"
+            max="300"
+          ></input>
+        </div>
+        <hr />
+        <div className="view-distance">
+          <label>
+            <span style={{ fontSize: 20 }}>Distances: </span>
+          </label>
+          <div className="show-hide-distances">
+            <p style={{ margin: 0 }}>Show</p>
+            <input
+              type="checkbox"
+              value={checked}
+              onClick={handleViewDistance}
+              id="view-distance"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
   return (
     <div className="settings-container">
       <div className="distance">
@@ -38,82 +100,26 @@ export const Settings = ({ numberOfRows, setNumberOfRows }) => {
         </p>
       </div>
       <div className="options">
-        <div className="settings-detail">
-          <div className="detail">
-            <u>Rows </u>
-            {numberOfRows}
-          </div>
-          <div className="detail">
-            <u>Speed</u>
-            {speed}ms
-          </div>
-          <div className="detail">
-            <u>Distances</u>
-            {checked ? "Hidden" : "Shown"}
-          </div>
-        </div>
-        <div className="settings-gear">
-          <div
-            className="gear"
-            id={active ? "white" : "gray"}
-            onMouseEnter={handleEnterGear}
-          ></div>
-          <div
-            className="settings-menu"
-            style={{ visibility: active ? "visible" : "hidden" }}
-            onMouseLeave={handleMouseLeaveSettings}
-          >
-            <div className="settings">
-              <div className="settings-title">
-                <h2 style={{ margin: 0 }}>Settings</h2>
-              </div>
-
-              <hr />
-              <div className="slider">
-                <label for="slider">
-                  <span style={{ fontSize: 20 }}>Rows: </span>
-                </label>
-                <input
-                  onChange={handleChange}
-                  type="range"
-                  value={numberOfRows}
-                  id="slider"
-                  min="5"
-                  max="20"
-                ></input>
-              </div>
-              <hr />
-              <div className="speed">
-                <label for="speed">
-                  <span style={{ fontSize: 20 }}>Speed: </span>
-                </label>
-                <input
-                  onChange={handleChangeSpeed}
-                  type="range"
-                  value={speed}
-                  id="speed"
-                  name="speed"
-                  min="10"
-                  max="300"
-                ></input>
-              </div>
-              <hr />
-              <div className="view-distance">
-                <label>
-                  <span style={{ fontSize: 20 }}>Distances: </span>
-                </label>
-                <div className="show-hide-distances">
-                  <p style={{ margin: 0 }}>Show</p>
-                  <input
-                    type="checkbox"
-                    value={checked}
-                    onClick={handleViewDistance}
-                    id="view-distance"
-                  />
-                </div>
-              </div>
+        <div
+          className="settings-detail"
+          onMouseEnter={handleEnterGear}
+          onClick={handleClick}
+        >
+          <div className="detail-container">
+            <div className="detail">
+              <u>Rows </u>
+              {numberOfRows}
+            </div>
+            <div className="detail">
+              <u>Speed</u>
+              {speed}ms
+            </div>
+            <div className="detail">
+              <u>Distances</u>
+              {checked ? "Hidden" : "Shown"}
             </div>
           </div>
+          {settingsMenu}
         </div>
       </div>
     </div>
